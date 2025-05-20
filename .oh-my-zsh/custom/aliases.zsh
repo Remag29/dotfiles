6523ddff8419c5b8ac06464f0193a60b3f79ace3
Remag29 <73289897+Remag29@users.noreply.github.com>
2025-05-20 16:49:21 +0200

    Add PowerShell config

--- .oh-my-zsh/custom/aliases.zsh
+++
@@ -1,8 +0,0 @@
-# Alias perso
-alias ip='ip -c'
-alias ll="lsd -l --git"
-alias lt="lsd --tree --git"
-alias lla="lsd -al --git"
-alias llt="lsd -l --tree --git"
-alias cls="clear"
-alias watch="watch -c"
