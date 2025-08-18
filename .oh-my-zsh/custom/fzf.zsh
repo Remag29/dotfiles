f58962bbb314a4c29e78c68dd97804e30f51f6eb
Remag29 <73289897+Remag29@users.noreply.github.com>
2025-08-18 15:56:20 +0200

    Customize fzf default options

--- .oh-my-zsh/custom/fzf.zsh
+++ .oh-my-zsh/custom/fzf.zsh
@@ -1,7 +1,7 @@
 # FZF Configuration
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-export FZF_DEFAULT_OPTS="--layout=reverse --inline-info --height=80% --border=sharp"
+export FZF_DEFAULT_OPTS="--tmux center,80%,50% --layout reverse --inline-info --height 80% --border sharp --style full --preview 'fzf-preview.sh {}'"
 export FZF_COMPLETION_PATH_OPTS="--walker file,dir,follow,hidden --preview='bat --color=always {}'"
 export FZF_COMPLETION_DIR_COMMANDS="cd pushd rmdir tree"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
